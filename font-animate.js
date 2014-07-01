93026b2ff152a429e2756c89b6b520cc998a5b40
Iain Andrew <iainbruceandrew@gmail.com>
2014-07-01 21:23:24 +0100

    added callback that removes original element and temporary wrapper, and restores original property values

--- font-animate.js
+++ font-animate.js
@@ -10,22 +10,6 @@
             fontStyle: $this.css('font-style'),
             duration: 1000
 		}, options );
-		
-			/*$span = $this.children('span');
-
-		$span.after('<span class="after" style="position:absolute; top:0; left:0; opacity:0;"></span>');
-		var $after = $span.siblings('.after');
-
-		$after.html( $span.html() );
-		$after.css({
-			'font-family': options.fontFamily,
-			'font-size': options.fontSize,
-			'font-weight': options.fontWeight,
-			'font-style': options.fontStyle
-		});
-
-		$span.stop().animate({opacity:0}, {duration:1000});
-		$after.stop().animate({opacity:1}, {duration:1000});*/
 
 		$this.wrap('<div class="font-animate"/>');
 		var $wrap = $('.font-animate');
@@ -34,11 +18,9 @@
 			position: 'relative'
 		});
 
-		$this.after('<span class="after" style="position:absolute; top:0; left:0; opacity:0;"></span>');
+		$this.after('<span class="after"></span>');
 
 		var $after = $this.siblings('.after');
-
-		$after.html( $this.html() );
 
 		var attributes = $this.prop("attributes");
 
@@ -47,15 +29,30 @@
 		    $after.attr(this.name, this.value);
 		});
 
+		$after.html( $this.html() );
+
 		$after.css({
+			position: 'absolute',
+			top: 0,
+			left: 0, 
+			opacity: 0,
 			'font-family': options.fontFamily || defaults.fontFamily,
 			'font-size': options.fontSize || defaults.fontSize,
 			'font-weight': options.fontWeight || defaults.fontWeight,
 			'font-style': options.fontStyle || defaults.fontStyle
 		});
 
-		$this.stop().animate({opacity:0}, {duration:options.duration || defaults.duration});
-		$after.stop().animate({opacity:1}, {duration:options.duration || defaults.duration});
+		$this.stop().animate({opacity:0}, options.duration || defaults.duration);
+		$after.stop().animate({opacity:1}, options.duration || defaults.duration, function() {
+			$after.unwrap();
+			$after.removeClass('after');
+			$after.css({
+				position: $this.css('position'),
+				top: $this.css('top'),
+				left: $this.css('left')
+			});
+			$this.remove();
+		});
 
 	}
 }) (jQuery);
